e76315b5e796da9829a539ccdd30d5ec1a4e8d59
kashmira <kashmira293.rejoice@gmail.com>
2025-02-11 11:58:24 +0530

    chore : add Redux Toolkit and configure store with provider

---
+++ src/redux/store.ts
@@ -0,0 +1,33 @@
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  TypedUseSelectorHook,
+  useDispatch as useAppDispatch,
+  useSelector as useAppSelector,
+} from 'react-redux';
+import rootReducer from './rootReducer';
+
+
+
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+// Type for dispatching actions from the store
+export type AppDispatch = typeof store.dispatch;
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+      immutableCheck: false,
+    }),
+});
+
+const { dispatch } = store;
+
+const useSelector: TypedUseSelectorHook<RootState> = useAppSelector;
+
+const useDispatch = () => useAppDispatch<AppDispatch>();
+
+
+export { store, dispatch, useSelector, useDispatch };
